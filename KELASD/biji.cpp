11db16203ceb6912144b690f4832128fe08a119c
sudirboyy <sudirboyy@gmail.com>
2023-03-06 15:47:10 +0700

    First commit

---
+++ KELASD/biji.cpp
@@ -0,0 +1,23 @@
+//Library
+#include <iostream>
+using namespace std;
+
+//program main
+int main(){//begin
+
+    //numeric p,l,luas
+    double p,l,luas;
+    //display "masukan panjang"
+    cout<<"masukan panjang = ";
+    //accept p
+    cin>>p;
+    //display "masukan lebar"
+    cout<<"masukan lebar = ";
+    //accept l
+    cin>>l;
+    //compute luas=p*l
+    luas=p*l;
+    //display "Luas persegi panjang adalah", Luas
+    cout<<"luas persegi panjang adalah "<<luas;
+
+}//end
